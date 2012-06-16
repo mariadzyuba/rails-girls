3b89080d64887530f14c4b80f8addefceac1a3b0
Maria <cherchich@gmail.com>
2012-06-16 15:08:34 +0300

    my first rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '48f77815a40874ed87b9b7b2a421e32395e40af2ff70fd1dd1c79fc16a07c1520345fec3db7bf0c2621842c772c5786b039bbe1176e27b579c94b965d56d7d60'
